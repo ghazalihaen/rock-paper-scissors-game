ac5bf11132bd25bdbc87a9ed9cc260f77012e097
Nasakti <ghazali.ha.en@gmail.com>
2019-06-18 22:59:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function win(userChoice, computerChoice) {
         userScore_span.innerHTML = userScore;
         computerScore_span.innerHTML = computerScore;
         result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win! 🔥`;
-        setTimeout(() => munculMenang(), 3000)
+        setTimeout(() => munculMenang(), 2500)
     }
 
     else {
